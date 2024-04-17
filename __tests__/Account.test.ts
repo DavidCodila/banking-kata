ad5ad17a5d87ccb2d9e28cbdc97b118fbf111050
David <david@Davids-MacBook-Pro.local>
2024-04-17 15:23:07 +1000

    Created withdraw test that fails

--- __tests__/Account.test.ts
+++ __tests__/Account.test.ts
@@ -1,8 +1,14 @@
 import { expect, test } from "bun:test";
 import { Account } from "../src/Account";
 
+const account = new Account();
+
 test("Test deposit method", () => {
-  const account = new Account();
   account.deposit(500);
   expect(account.balance).toEqual(500);
+});
+
+test("Test withdraw method", () => {
+  account.withdraw(100);
+  expect(account.balance).toEqual(400);
 });

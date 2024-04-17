d694b929be152f546a7d137f5938c46f5135ed14
David <david@Davids-MacBook-Pro.local>
2024-04-17 16:20:38 +1000

    Completed kata

--- src/Account.ts
+++ src/Account.ts
@@ -1,14 +1,23 @@
 export class Account {
   public balance = 0;
-  private date = "0/0/0000";
+  private date!: string;
+  private events = ["Date \t\t Amount \t Balance "];
   construstor() {}
   deposit(amountToDeposit: number): void {
     this.balance += amountToDeposit;
+    this.date = String(new Date()).split(" ", 4).join(" ");
+    this.events.push(
+      this.date + " \t " + "+" + amountToDeposit + " \t\t " + this.balance
+    );
   }
   withdraw(amountToWithdraw: number): void {
     this.balance -= amountToWithdraw;
+    this.events.push(
+      this.date + " \t " + "-" + amountToWithdraw + " \t\t " + this.balance
+    );
   }
   printStatement(): String {
-    return "";
+    const statement = this.events.join("\n");
+    return String(statement);
   }
 }
